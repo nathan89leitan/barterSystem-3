93d88dfd40a1b104a8f83c29afd82107823adc27
nathan89leitan <66981818+nathan89leitan@users.noreply.github.com>
2021-05-24 16:56:38 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Login from './screens/login'
+export default function App() {
+  return (
+    <Login/>
+  );
+}
